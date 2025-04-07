d5767a0bd2aed1249b73032ca857dfbda55329ab
alifnrz27 <alifnaufalrizki27@gmail.com>
2025-04-07 14:50:34 +0700

    edit DB

--- database/migrations/2025_01_14_090238_create_courses_table.php
+++ database/migrations/2025_01_14_090238_create_courses_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
         $table->text('description');
         $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
         $table->string('image_url')->nullable();
-        $table->foreignId('category_id')->constrained()->onDelete('cascade');
+        $table->foreignId('category_id')->nullable();
         $table->timestamps();
     });
 }
